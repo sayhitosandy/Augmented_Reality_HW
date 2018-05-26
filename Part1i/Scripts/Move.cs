95e85f244ea2fb8cbe435acffccead572cb31dfb
sayhitosandy <sanidhya15085@iiitd.ac.in>
2018-05-26 18:40:50 +0530

    Added files via Upload

---
+++ Part1i/Scripts/Move.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+    //Animator anim;
+    float speed = 0.1f;
+    float turnSpeed = 1.0f;
+    public bool dead = false;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "bullet")
+        {
+            Hit();
+        }
+        else if (col.gameObject.tag == "home")
+        {
+            Destroy(this.gameObject, 1);
+        }
+    }
+
+    public void Hit()
+    {
+        dead = true;
+        //anim.setTrigger("IsDying");
+        Destroy(this.GetComponent<Collider>(), 1);
+        Destroy(this.GetComponent<Rigidbody>(), 1);
+        Destroy(this.gameObject, 2);
+    }
+
+	// Use this for initialization
+	void Start () {
+        //anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dead)
+        {
+            return;
+        }
+
+        GameObject home = GameObject.FindWithTag("home");
+        if (home != null)
+        {
+            Vector3 direction = home.transform.position - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), turnSpeed * Time.smoothDeltaTime);
+
+        }
+
+        this.transform.Translate(0, 0, speed);
+	}
+}
---
+++ Part1i/Scripts/TowerBehaviour.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBehaviour : MonoBehaviour {
+
+    public GameObject bullet;
+    public Transform spawnPos;
+    GameObject troll;
+
+    void OnTriggerEnter(Collider obj)
+    {
+        if (obj.gameObject.tag == "troll" && troll == null)
+        {
+            troll = obj.gameObject;
+            InvokeRepeating("Shoot", 0, 1.0f);
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bullet, spawnPos.position, spawnPos.rotation);
+        if (troll.GetComponent<Move>().dead)
+        {
+            troll = null;
+            CancelInvoke("Shoot");
+        }
+    }
+
+    void OnTriggerExit(Collider obj)
+    {
+        if (obj.gameObject == troll)
+        {
+            troll = null;
+            CancelInvoke("Shoot");
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (troll != null)
+        {
+            this.transform.LookAt(troll.transform.position);
+        }
+	}
+}
